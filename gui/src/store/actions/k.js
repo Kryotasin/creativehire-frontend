6b186db6dc1abcca4a097afb818e77e882836e38
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-03-21 15:20:58 -0700

    Delete k.js

--- gui/src/store/actions/k.js
+++
@@ -1,7 +0,0 @@
-<SubMenu style={{float: 'right'}} title={<span><PlusOutlined /> <Link to="create"> New Jobpost</Link></span>}>
-                            <MenuItemGroup title="Item 1">
-                                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                <Menu.Item key="setting:2">Option 2</Menu.Item>
-                            </MenuItemGroup>
-                            
-                        </SubMenu>
