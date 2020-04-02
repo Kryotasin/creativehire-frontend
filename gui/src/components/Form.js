a98851f529b25404ffed2364d6504bc17e4fa8a8
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-04-02 15:32:02 -0700

    Commented console logs

--- gui/src/components/Form.js
+++ gui/src/components/Form.js
@@ -22,7 +22,7 @@ const validateMessages = {
 class CustomForm extends React.Component {
 
     componentDidMount = () => {
-      console.log(this.props);
+      // console.log(this.props);
     }
 
     handleFormSubmit = (event, requestType, jobpostID) => {
