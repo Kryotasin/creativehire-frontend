a6d013bfaa872cb401db8b6d0953ef2ab6316e95
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-04-18 18:32:53 -0700

    Create CandidateHome.js

---
+++ gui/src/containers/CandidateHome.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Jobposts from '../components/Jobpost';
+
+import axios from 'axios';
+
+
+class JobpostList extends React.Component{
+
+    state = {
+        jobposts: []
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/jobpost/')
+            .then(res => {
+                this.setState({
+                    jobposts: res.data
+                });
+                //console.log(res.data);
+            })
+    }
+
+    render(){
+        return (
+            <div>
+                 <Jobposts data={this.state.jobposts} />
+                <br />
+            </div>
+            
+        )
+    }
+}
+
+export default JobpostList;
