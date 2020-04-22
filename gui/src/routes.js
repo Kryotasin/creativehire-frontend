a9f8c4ad9c528fbf91a539c56ae5094eb69deb8f
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-04-22 08:35:46 -0700

    Update routes.js

--- gui/src/routes.js
+++ gui/src/routes.js
@@ -8,6 +8,7 @@ import JobpostCreate from './containers/JobpostCreate';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import LandingPage from './containers/LadingPage';
+import UserProfile from './containers/Profile';
 
 const BaseRouter = () => (
     <div>
@@ -17,6 +18,7 @@ const BaseRouter = () => (
         <Route exact path ='/login/' component ={Login} />
         <Route exact path ='/signup/' component ={Signup} />
         <Route exact path ='/home/' component ={LandingPage} />
+        <Route exact path ='/profile/' component ={UserProfile} />
     </div>
 );
 
