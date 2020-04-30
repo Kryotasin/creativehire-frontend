e5fdcfff68f44a5347173790b5ccbaeee33a12b0
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-04-30 02:39:00 -0700

    Create Matches.js

---
+++ gui/src/containers/Matches.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Form, Input, Button, DatePicker, Upload, message } from 'antd';
+
+
+import axios from 'axios';
+
+
+
+class Comparison extends React.Component{
+
+    componentDidMount() {
+
+        
+    }
+
+
+
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
