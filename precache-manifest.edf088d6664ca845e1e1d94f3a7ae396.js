7a01251985531bde45ecc20d99fe4d4adfbaf715
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-05-03 12:40:25 -0700

    Updates

---
+++ precache-manifest.edf088d6664ca845e1e1d94f3a7ae396.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4dd24b2acaf5e6931d74e2c238dec7",
+    "url": "/creativehire-frontend/index.html"
+  },
+  {
+    "revision": "a5e2b36709d3aba73929",
+    "url": "/creativehire-frontend/static/css/2.0e1745bb.chunk.css"
+  },
+  {
+    "revision": "2f27c73dea78f6117b57",
+    "url": "/creativehire-frontend/static/css/main.d68ae2d0.chunk.css"
+  },
+  {
+    "revision": "a5e2b36709d3aba73929",
+    "url": "/creativehire-frontend/static/js/2.4722c6e1.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/creativehire-frontend/static/js/2.4722c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f27c73dea78f6117b57",
+    "url": "/creativehire-frontend/static/js/main.5228ae81.chunk.js"
+  },
+  {
+    "revision": "9054187077b2f50dbe87",
+    "url": "/creativehire-frontend/static/js/runtime-main.33020a6e.js"
+  },
+  {
+    "revision": "eb89aec13c3601dd1c3c695b67fb2fa5",
+    "url": "/creativehire-frontend/static/media/logo.eb89aec1.svg"
+  }
+]);
