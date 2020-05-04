750049993cb339356375bc4f68ec504a677b04cc
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-05-04 16:53:14 -0700

    Build v2 Windows

---
+++ gui/build/precache-manifest.da07e7d97127e86b67877bb68d746bed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e55bb4ddf5c1dd78f08be20e3f8a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2dab5eba25dcaeb463f",
+    "url": "/static/css/2.0e1745bb.chunk.css"
+  },
+  {
+    "revision": "b508add1720e6c9c892a",
+    "url": "/static/css/main.d68ae2d0.chunk.css"
+  },
+  {
+    "revision": "a2dab5eba25dcaeb463f",
+    "url": "/static/js/2.178f04eb.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.178f04eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b508add1720e6c9c892a",
+    "url": "/static/js/main.e6c9c0bf.chunk.js"
+  },
+  {
+    "revision": "171aa2dbd02624706eab",
+    "url": "/static/js/runtime-main.30697f77.js"
+  },
+  {
+    "revision": "eb89aec13c3601dd1c3c695b67fb2fa5",
+    "url": "/static/media/logo.eb89aec1.svg"
+  }
+]);
