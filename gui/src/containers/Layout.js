b1e30dcaf40cb360491b8870a1a510511987cfdd
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-05-09 10:15:16 -0700

    Update Layout.js

--- gui/src/containers/Layout.js
+++ gui/src/containers/Layout.js
@@ -7,91 +7,77 @@ import logo from '../assets/logo.svg';
 
 import { Link } from 'react-router-dom';
 
-const { Header, Content, Footer } = Layout;
 
-
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+const { Header, Content, Footer, Sider } = Layout;
 
 
 class CustomLayout extends React.Component {
+
+    state = {
+        collapsed: false
+    }
+
+    onCollapse = collapsed => {
+        this.setState({ collapsed });
+      };
+
 
     componentDidMount() {
       }
 
     render() {
         return(
-            <Layout className="layout">
-                <Header>
-                <div className="logo" />
-    
-    
-                    
 
+    <Layout style={{ minHeight: '100vh' }}>
+          <div className="logo" />
 
-                        {
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} breakpoint="md">
+          {
                             localStorage.getItem('token') !== null && localStorage.getItem('error') == null?
                             <Menu
                             theme="dark"
-                            mode="horizontal"
+                            mode="inline"
                             defaultSelectedKeys={['10']}
                             style={{ lineHeight: '64px' }}
-                            >
-
+                            >                            
                                 <Menu.Item key="6"><Link to="/"><img src={logo} style={{width: '9rem'}}></img></Link></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="5" onClickCapture={this.props.logout}> Logout <LogoutOutlined /></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="4"><Link to="/profile/">Profile <UserOutlined /></Link></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="3"><Link to="/how-it-works/">How it works <QuestionCircleOutlined /></Link></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="2"><Link to="/my-scans">My Scans <FolderOpenOutlined /></Link></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="1"><Link to="/new-scan">New Scan <PlusCircleOutlined /> </Link></Menu.Item>
+                                <Menu.Item key="1" icon={<PlusCircleOutlined />}><Link to="/new-scan">New Scan  </Link></Menu.Item>
+                                <Menu.Item key="2" icon={<FolderOpenOutlined />}><Link to="/my-scans">My Scans </Link></Menu.Item>
+                                <Menu.Item key="3" icon={<QuestionCircleOutlined />}><Link to="/how-it-works/">How it works </Link></Menu.Item>
+                                <Menu.Item key="4" icon={<UserOutlined />}><Link to="/profile/">Profile </Link></Menu.Item>
+                                <Menu.Item key="5" onClickCapture={this.props.logout} icon={<LogoutOutlined />}> Logout </Menu.Item>
+
+
                                 </Menu>
                             :
                             <Menu
                             theme="dark"
-                            mode="horizontal"
+                            mode="inline"
                             defaultSelectedKeys={['']}
                             style={{ lineHeight: '64px' }}
                             >
 
-                                <Menu.Item key="6"><Link to="/"><img src={logo} style={{width: '9rem'}}></img></Link></Menu.Item>
-                                <Menu.Item style={{float: 'right'}} key="2"><Link to="/login">Login</Link></Menu.Item>
+                                <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
                                 </Menu>
 
                         }
-                        {
-                            // localStorage.getItem('token') !== null && localStorage.getItem('error') == null?
-                            //     <Menu.Item key="2"><Link to="/jobs">Jobs</Link></Menu.Item>
-                            
-                            // :
-                            // ''
-                        }
-                            
-                        {
-                            // localStorage.getItem('token') !== null && localStorage.getItem('error') == null?
-                            // <SubMenu style={{float: 'right'}} title={<span><Link to="/profile/"> Profile</Link></span>}>
-                            //     <MenuItemGroup title="General">
-                            //         <Menu.Item key="setting:2"><span><PlusOutlined /> <Link to="/create/"> New Jobpost</Link></span></Menu.Item>
-                            //         <Menu.Item key="setting:3" onClickCapture={this.props.logout}><LogoutOutlined /> Logout</Menu.Item>
-                            //     </MenuItemGroup>
-                            // </SubMenu>
-                          
-                            //     :
-    
-                            //     <Menu.Item style={{float: 'right'}} key="2"><Link to="/login">Login</Link></Menu.Item>
-                        }
-                        
+        </Sider>
 
-    
-    
-                </Header>
-                <Content style={{ padding: '50px' }}>
-                <div className="site-layout-content">
-                    {this.props.children}
-                </div>
-    
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>CreativeHire 2020</Footer>
-            </Layout>
+
+
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          
+          <Content style={{ margin: '0 16px', padding: '50px' }}>
+            <div className="site-layout-content">
+                {this.props.children}
+            </div>
+          </Content>
+
+          <Footer style={{ textAlign: 'center' }}>CreativeHire 2020</Footer>
+        </Layout>
+
+      </Layout>
         );
     }
 
