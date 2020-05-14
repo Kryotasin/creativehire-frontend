e973116d676f9a842f17e343b7232fd30668734f
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-05-14 02:02:40 -0700

    All changes

---
+++ gui/build/precache-manifest.2ddc3186c956ccc4896cef1ac2efa060.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3e4ff8484edeb464ddbd020be4bcd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02e7c6d7531d6e0e273c",
+    "url": "/static/css/2.601f2d5d.chunk.css"
+  },
+  {
+    "revision": "db10e24ad8021db7c21c",
+    "url": "/static/css/main.d68ae2d0.chunk.css"
+  },
+  {
+    "revision": "02e7c6d7531d6e0e273c",
+    "url": "/static/js/2.b8f81cc9.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.b8f81cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db10e24ad8021db7c21c",
+    "url": "/static/js/main.a1c7758b.chunk.js"
+  },
+  {
+    "revision": "171aa2dbd02624706eab",
+    "url": "/static/js/runtime-main.30697f77.js"
+  },
+  {
+    "revision": "eb89aec13c3601dd1c3c695b67fb2fa5",
+    "url": "/static/media/logo.eb89aec1.svg"
+  }
+]);
