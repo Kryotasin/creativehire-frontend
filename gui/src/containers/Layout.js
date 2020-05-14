b37a5880c1e353be3aceed8db0c35ff72feae70b
Kshitij Sinha <kshitij.sinha94@gmail.com>
2020-05-13 20:54:40 -0700

    Update Layout.js

--- gui/src/containers/Layout.js
+++ gui/src/containers/Layout.js
@@ -2,7 +2,7 @@ import React from'react';
 import * as actions from '../store/actions/auth';
 import {connect} from 'react-redux';
 import { Menu, Layout } from 'antd';
-import { PlusCircleOutlined, FolderOpenOutlined, LogoutOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined, FolderOpenOutlined, LoginOutlined, LogoutOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
 import logo from '../assets/logo.svg';
 
 import { Link } from 'react-router-dom';
@@ -21,10 +21,6 @@ class CustomLayout extends React.Component {
         this.setState({ collapsed });
       };
 
-
-    componentDidMount() {
-      }
-
     render() {
         return(
 
@@ -33,34 +29,30 @@ class CustomLayout extends React.Component {
 
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} breakpoint="md">
           {
-                            localStorage.getItem('token') !== null && localStorage.getItem('error') == null?
-                            <Menu
-                            theme="dark"
-                            mode="inline"
-                            defaultSelectedKeys={['10']}
-                            style={{ lineHeight: '64px' }}
-                            >                            
-                                <Menu.Item key="6"><Link to="/"><img alt="Creativehire logo" src={logo} style={{width: '9rem'}}></img></Link></Menu.Item>
-                                <Menu.Item key="1" icon={<PlusCircleOutlined />}><Link to="/new-scan">New Scan  </Link></Menu.Item>
-                                <Menu.Item key="2" icon={<FolderOpenOutlined />}><Link to="/my-scans">My Scans </Link></Menu.Item>
-                                <Menu.Item key="3" icon={<QuestionCircleOutlined />}><Link to="/how-it-works/">How it works </Link></Menu.Item>
-                                <Menu.Item key="4" icon={<UserOutlined />}><Link to="/profile/">Profile </Link></Menu.Item>
-                                <Menu.Item key="5" onClickCapture={this.props.logout} icon={<LogoutOutlined />}> Logout </Menu.Item>
-
-
-                                </Menu>
-                            :
-                            <Menu
-                            theme="dark"
-                            mode="inline"
-                            defaultSelectedKeys={['']}
-                            style={{ lineHeight: '64px' }}
-                            >
-
-                                <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
-                                </Menu>
-
-                        }
+              localStorage.getItem('token') !== null && localStorage.getItem('error') == null?
+              <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['10']}
+              style={{ lineHeight: '64px' }}
+              >                            
+                  <Menu.Item key="6"><Link to="/"><img alt="Creativehire logo" src={logo} style={{width: '9rem'}}></img></Link></Menu.Item>
+                  <Menu.Item key="1" icon={<PlusCircleOutlined />}><Link to="/new-scan">New Scan  </Link></Menu.Item>
+                  <Menu.Item key="2" icon={<FolderOpenOutlined />}><Link to="/my-scans">My Scans </Link></Menu.Item>
+                  <Menu.Item key="3" icon={<QuestionCircleOutlined />}><Link to="/how-it-works/">How it works </Link></Menu.Item>
+                  <Menu.Item key="4" icon={<UserOutlined />}><Link to="/profile/">Profile </Link></Menu.Item>
+                  <Menu.Item key="5" onClickCapture={this.props.logout} icon={<LogoutOutlined />}> Logout </Menu.Item>
+                  </Menu>
+              :
+              <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['']}
+              style={{ lineHeight: '64px' }}
+              >
+                  <Menu.Item key="2" icon={<LoginOutlined />}><Link to="/login">Login</Link></Menu.Item>
+                  </Menu>
+          }
         </Sider>
 
 
