ff50de285ef933c07c6d8a782858420c74df8334
Kshitij <kshitij.sinha94@gmail.com>
2020-05-25 01:42:22 -0700

    Preview image

--- gui/src/App.js
+++ gui/src/App.js
@@ -11,6 +11,9 @@ import CustomLayout from './containers/Misc/Layout';
 import GuestRouter from './guestRoute';
 import ErrorBoundary from './components/ErrorBoundary'
 
+import preview from './assets/preview.png';
+import {Helmet} from "react-helmet";
+
 class App extends Component {
 
   componentDidMount() {
@@ -21,7 +24,17 @@ class App extends Component {
   render() {
     return (
       <div className="">
-
+  
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Creativehire</title>
+        <meta name="description" content="Creativehire website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={preview} /> 
+        <meta property="og:url" content="https://www.creativehire.co/" />
+        <meta property="og:title" content="Creativhire Home" />
+        <meta property="og:description" content="Creativhire - Evaluate the strength of your portfolio" />
+      </Helmet>
           {
             localStorage.getItem('token') !== null ?
             <Router basename="/">
